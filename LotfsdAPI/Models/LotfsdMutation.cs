6dcf4a76a7f80730736a16944c066c0b7a43c6bd
Janne Lavila <janne.lavila@wheelq.com>
2019-12-30 20:36:28 +0200

    doodling with graphql server, limited success

--- LotfsdAPI/Models/LotfsdMutation.cs
+++ LotfsdAPI/Models/LotfsdMutation.cs
@@ -1,11 +1,16 @@
+using GraphQL;
+using GraphQL.Server.Transports.Subscriptions.Abstractions;
 using GraphQL.Types;
 using LotfsdAPI.Services;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
 
 namespace LotfsdAPI.Models
 {
   public class LotfsdMutation : ObjectGraphType
   {
-    public LotfsdMutation(CharacterSheetService characterSheetService)
+    public LotfsdMutation(CharacterSheetService characterSheetService, IHttpContextAccessor httpContextAccessor)
     {
       Name = "Mutation";
 
@@ -17,6 +22,8 @@ namespace LotfsdAPI.Models
         resolve: context =>
         {
           var characterSheet = context.GetArgument<CharacterSheet>("characterSheet");
+          var user = httpContextAccessor.HttpContext.User;
+          characterSheet.Owner = user.FindFirst(ClaimTypes.Name).Value;
           return characterSheetService.Create(characterSheet);
         });
     }
