b751c22f5a0a34148efb70c452deaa2ee271e53f
Janne Lavila <janne.lavila@wheelq.com>
2020-01-05 14:33:48 +0200

    added a bunch of queries

--- LotfsdTypes/LotfsdQuery.cs
+++ LotfsdTypes/LotfsdQuery.cs
@@ -1,46 +1,128 @@
 using GraphQL.Types;
 using Lotfsd.Data;
 using Lotfsd.Data.Models;
+using System.Collections.Generic;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Lotfsd.Types.Models
 {
   public class LotfsdQuery : ObjectGraphType
   {
-    public LotfsdQuery(CharacterSheetService<Info> infoService)
+    private Task<T> SingleResolver<T>(
+      ResolveFieldContext<object> context,
+      CharacterSheetService<T> service
+      ) where T : ICharacterSheet
+    {
+      var userContext = context.UserContext as GraphQLUserContext;
+      var owner = userContext.User.FindFirst(ClaimTypes.Name).Value;
+      return service.Get(owner, context.GetArgument<string>("id"));
+    }
+
+    private Task<List<T>> ManyResolver<T>(
+      ResolveFieldContext<object> context,
+      CharacterSheetService<T> service
+      ) where T : ICharacterSheet
+    {
+      var userContext = context.UserContext as GraphQLUserContext;
+      var owner = userContext.User.FindFirst(ClaimTypes.Name).Value;
+      return service.Get(owner);
+    }
+
+    public LotfsdQuery(
+      CharacterSheetService<Info> infoService,
+      CharacterSheetService<Attributes> attributesService,
+      CharacterSheetService<AttributeModifiers> attributeModifiersService,
+      CharacterSheetService<SavingThrows> savingThrowsService,
+      CharacterSheetService<CommonActivities> commonActivitiesService,
+      CharacterSheetService<Wallet> walletService,
+      CharacterSheetService<Effect> effectService,
+      CharacterSheetService<Retainer> retainerService,
+      CharacterSheetService<CombatOptions> combatOptionsService
+      )
     {
       Name = "Query";
 
       Field<InfoType>(
         "info",
         arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
-        resolve: context =>
-          {
-            var userContext = context.UserContext as GraphQLUserContext;
-            var owner = userContext.User.FindFirst(ClaimTypes.Name).Value;
-            return infoService.Get(owner, context.GetArgument<string>("id"));
-          });
+        resolve: context => SingleResolver(context, infoService));
 
       Field<ListGraphType<InfoType>>(
         "infos",
-        resolve: context =>
-        {
-          var userContext = context.UserContext as GraphQLUserContext;
-          var owner = userContext.User.FindFirst(ClaimTypes.Name).Value;
-          return infoService.Get(owner);
-        });
+        resolve: context => ManyResolver(context, infoService));
 
-      Field<InfoType>(
-        "foo",
-        resolve: context => new Info
-        {
-          Id = "foo",
-          Name = "bar",
-          Owner = "baz",
-          Age = 0,
-          Alignment = "chaotic",
-          AttackBonus = 3
-        });
+      Field<AttributesType>(
+        "attributes",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, attributesService));
+
+      Field<ListGraphType<AttributesType>>(
+        "allAttributes",
+        resolve: context => ManyResolver(context, attributesService));
+
+      Field<AttributeModifiersType>(
+        "attributeModifiers",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, attributeModifiersService));
+
+      Field<ListGraphType<AttributeModifiersType>>(
+        "allAttributeModifiers",
+        resolve: context => ManyResolver(context, attributeModifiersService));
+
+      Field<SavingThrowsType>(
+        "savingThrows",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, savingThrowsService));
+
+      Field<ListGraphType<SavingThrowsType>>(
+        "allSavingThrows",
+        resolve: context => ManyResolver(context, savingThrowsService));
+
+      Field<ListGraphType<CommonActivitiesType>>(
+        "commonActivities",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, commonActivitiesService));
+
+      Field<ListGraphType<CommonActivitiesType>>(
+        "allCommonActivities",
+        resolve: context => ManyResolver(context, commonActivitiesService));
+
+      Field<WalletType>(
+        "wallet",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, walletService));
+
+      Field<ListGraphType<WalletType>>(
+        "wallets",
+        resolve: context => ManyResolver(context, walletService));
+
+      Field<EffectType>(
+        "effect",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, effectService));
+
+      Field<ListGraphType<InfoType>>(
+        "effects",
+        resolve: context => ManyResolver(context, effectService));
+
+      Field<RetainerType>(
+        "retainer",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, retainerService));
+
+      Field<ListGraphType<InfoType>>(
+        "retainers",
+        resolve: context => ManyResolver(context, retainerService));
+
+      Field<CombatOptionsType>(
+        "combatOptions",
+        arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "id" }),
+        resolve: context => SingleResolver(context, combatOptionsService));
+
+      Field<ListGraphType<InfoType>>(
+        "allCombatOptions",
+        resolve: context => ManyResolver(context, combatOptionsService));
     }
   }
 }
